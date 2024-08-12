633f18610d61b7f8e4ec0b87f973450127ef3f2a
AndresRagua <88854825+AndresRagua@users.noreply.github.com>
2024-08-12 11:11:54 -0400

    Confirmacion VE I

--- core/config.py
+++ core/config.py
@@ -2,15 +2,6 @@ import os
 from dotenv import load_dotenv
 from pathlib import Path
 
-# Determina el entorno de ejecución (local o producción)
-ENVIRONMENT = os.getenv('ENVIRONMENT', 'local')
-
-# Cargar el archivo .env adecuado según el entorno
-if ENVIRONMENT == 'production':
-    load_dotenv(dotenv_path=Path('.') / '.env.production')
-else:
-    load_dotenv(dotenv_path=Path('.') / '.env.local')
-
 class Setting:
     DATABASE_URL = os.getenv("DATABASE_URL")
 
