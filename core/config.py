1b0987b575a54b403843942a72c468f26549ff7b
AndresRagua <88854825+AndresRagua@users.noreply.github.com>
2024-08-13 18:20:02 -0400

    Actualizacion Variables de entorno global y local

--- core/config.py
+++ core/config.py
@@ -2,12 +2,30 @@ import os
 from dotenv import load_dotenv
 from pathlib import Path
 
-class Setting:
-    DATABASE_URL = os.getenv("DATABASE_URL")
+# Determina si estás en desarrollo o producción
+ENVIRONMENT = os.getenv('ENVIRONMENT', 'production')  # Por defecto es 'production'
 
+if ENVIRONMENT == 'development':
+    # Cargar variables de entorno desde el archivo .env.local en desarrollo
+    env_path = Path('.env.local')
+    load_dotenv(dotenv_path=env_path)
+    print("Cargando variables desde .env.local")
+else:
+    print("Cargando variables desde el entorno de producción")
+
+# Clase para la configuración de la base de datos
+class Setting:
+    DATABASE_URL: str = os.getenv("DATABASE_URL")
+# Instancia de configuración de la base de datos
 settings = Setting()
 
+# Clase para la configuración de la URL del frontend
 class SettingHost:
     FRONTEND_URL: str = os.getenv("FRONTEND_URL")
 
+# Instancia de configuración del host
 settingsHost = SettingHost()
+
+# Accede a las variables de entorno como atributos de tus instancias
+print(settings.DATABASE_URL)
+print(settingsHost.FRONTEND_URL)
